b0a4bf03c12179172a7fbceacb17408fdda67189
Guillermo Esteves <gfesteves@gmail.com>
2011-09-19 14:10:22 -0430

    Site updated at 2011-09-19 18:40:22 UTC

---
+++ public/javascripts/application.js
@@ -0,0 +1,23 @@
+twttr.events.bind('follow', trackFollow);
+twttr.events.bind('tweet', trackTweet);
+function trackFollow(intent_event) {
+    if (intent_event) {
+      var label = intent_event.data.screen_name;
+      _gaq.push(['_trackSocial', 'Twitter', 'Follow', label]);
+    }
+  }
+function trackTweet(intent_event) {
+    if (intent_event) {
+      var label = location.href;
+      _gaq.push(['_trackSocial', 'Twitter', 'Tweet', label]);
+    }
+  }
+FB.Event.subscribe('edge.create', function(targetUrl) {
+  _gaq.push(['_trackSocial', 'Facebook', 'Like', targetUrl]);
+});
+FB.Event.subscribe('edge.remove', function(targetUrl) {
+  _gaq.push(['_trackSocial', 'Facebook', 'Unlike', targetUrl]);
+});
+FB.Event.subscribe('message.send', function(targetUrl) {
+  _gaq.push(['_trackSocial', 'Facebook', 'Send', targetUrl]);
+});
