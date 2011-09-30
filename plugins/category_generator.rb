a03e29525e801646311570a566aac9673d8258a8
Guillermo Esteves <gfesteves@gmail.com>
2011-09-30 10:22:50 -0430

    Change category page titles.

--- plugins/category_generator.rb
+++ plugins/category_generator.rb
@@ -40,10 +40,10 @@ module Jekyll
       self.data['category']    = category
       # Set the title for this page.
       title_prefix             = site.config['category_title_prefix'] || 'Category: '
-      self.data['title']       = "#{title_prefix}#{category}"
+      self.data['title']       = "Posts tagged “#{category}”"
       # Set the meta-description for this page.
       meta_description_prefix  = site.config['category_meta_description_prefix'] || 'Category: '
-      self.data['description'] = "#{meta_description_prefix}#{category}"
+      self.data['description'] = "Posts tagged “#{category}”"
     end
 
   end
